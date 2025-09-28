bad1b401aa44c93f60f659490acec14a008e0107
samjoy247 <samijoy111@gmail.com>
2025-09-29 02:10:59 +0600

    User attributes

--- Learn2Earn/users/models.py
+++ Learn2Earn/users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
