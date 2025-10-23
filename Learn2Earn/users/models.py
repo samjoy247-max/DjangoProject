9a2c95b878c99d1948c0becd9ed8c234aed49ab8
samjoy247-max <23101102@uap-bd.edu>
2025-10-23 19:07:14 +0600

    Update models.py

--- Learn2Earn/users/models.py
+++ Learn2Earn/users/models.py
@@ -1,7 +1,25 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=20)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Service(models.Model):
+    provider = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=20)
+    price_per_hour = models.FloatField()
+    description = models.TextField()
+    status = models.CharField(max_length=20)
+    service_image = models.ImageField(upload_to='service_pics/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
